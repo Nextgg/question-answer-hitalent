c5f86f070e1ecc18105f4440147afcf3653f0003
Владислав Лысых <hdnext2@gmail.com>
2025-09-04 19:49:33 +0300

    add models and schems

--- src/answer/schemas.py
+++ src/answer/schemas.py
@@ -1,12 +1,4 @@
-from database import Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from datetime import datetime
+from pydantic import BaseModel
 
-class Answer(Base):
-    __tablename__ = "answer"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    question_id = Column(ForeignKey("question.id"))
-    user_id = Column(Integer)
-    text = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.now)
+class answer(BaseModel):
+    id: int
